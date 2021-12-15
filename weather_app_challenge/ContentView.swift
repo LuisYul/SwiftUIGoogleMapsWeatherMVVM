35f2f09b6f62399a1fc6b5330a26db410b48edc9
LuisYulB <lbaltodano@proveedor.intercorp.com.pe>
2021-12-15 01:03:32 -0500

    Initial Commit

---
+++ weather_app_challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weather_app_challenge
+//
+//  Created by Luis Yul Baltodano Marquez on 15/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weather_app_challenge/weather_app_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_app_challengeApp.swift
+//  weather_app_challenge
+//
+//  Created by Luis Yul Baltodano Marquez on 15/12/21.
+//
+
+import SwiftUI
+
+@main
+struct weather_app_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
